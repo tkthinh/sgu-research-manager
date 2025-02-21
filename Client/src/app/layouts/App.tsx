1a0b1c3d9bc4d54a5a1f10428e4242ffe6d94ac0
thinh tu <khangthinhproc@gmail.com>
2025-02-21 17:35:44 +0700

    restructuring project

---
+++ Client/src/app/layouts/App.tsx
@@ -0,0 +1,10 @@
+import Sidebar from "./Sidebar";
+
+export default function App() {
+  return (
+    <>
+      <Sidebar></Sidebar>
+      <div>App</div>
+    </>
+  )
+}
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import './index.css';
+import './app/layouts/styles.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import App from './App.tsx';
+import App from './app/layouts/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
