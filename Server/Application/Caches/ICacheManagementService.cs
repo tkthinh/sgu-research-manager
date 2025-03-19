0b021be5d03b9cee87914d2cbbe8142c2c5116b7
thinh tu <khangthinhproc@gmail.com>
2025-03-19 13:55:38 +0700

    add cache management functionality

---
+++ Server/Application/Caches/ICacheManagementService.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Caches
+{
+   public interface ICacheManagementService
+   {
+      Task<IEnumerable<string>> GetAllKeysAsync(CancellationToken cancellationToken = default);
+      Task ClearCacheAsync(string key, CancellationToken cancellationToken = default);
+      Task ClearAllCacheAsync(CancellationToken cancellationToken = default);
+   }
+}
